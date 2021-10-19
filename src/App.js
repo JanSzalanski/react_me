fc8bd2dfbdba071b9316c53d5192d3665396cf37
JanSzalanski <jan@szalanski.eu>
2021-10-19 18:57:23 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'
 import './App.css';
 
-function App() {
+class App() extends React.Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,16 +20,10 @@ function App() {
           Learn React LoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
           dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-          anim id est laborum tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-          deserunt mollit anim id est laborum
         </a>
       </header>
     </div>
   );
 }
-
+}
 export default App;
