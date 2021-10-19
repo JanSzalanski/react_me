6b69fa2db2990d4d1c9976e505a85b28ee83a456
JanSzalanski <jan@szalanski.eu>
2021-10-19 21:27:41 +0200

    test husky

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg'
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App() extends React.Component {
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React LoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        </a>
-      </header>
-    </div>
-  );
-}
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React LoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </a>
+    </header>
+  </div>
+);
+
 export default App;
