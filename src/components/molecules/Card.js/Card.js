6797e95e2a1a2740b4a2a51fd24f043018fd60cc
JanSzalanski <jan@szalanski.eu>
2021-11-02 22:47:01 +0100

    added Card molecul

---
+++ src/components/molecules/Card.js/Card.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Heading from 'components/atoms/Heading/Heading';
+import Button from 'components/atoms/Button/Button';
+
+const StyledWrapper = styled.div`
+  min-height: 380px;
+  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.1);
+  border-radius: 8px;
+  overflow: hidden;
+  display: grid;
+  grid-template-rows: 0.25fr 1fr;
+`;
+
+const InnerWrapper = styled.div`
+  padding: 17px 30px;
+  background-color: ${({ blue, theme }) => (blue ? theme.primary : 'white')};
+
+  ${({ flex }) =>
+    flex &&
+    css`
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+    `}
+`;
+
+const DateInfo = styled(Paragraph)`
+  margin: 0 0 5px;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 5px 0 0;
+`;
+
+const Card = () => (
+  <StyledWrapper>
+    <InnerWrapper blue>
+      <StyledHeading>Hello react!</StyledHeading>
+      <DateInfo>3 days</DateInfo>
+    </InnerWrapper>
+    <InnerWrapper flex>
+      <Paragraph>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti error enim corporis
+        tempora praesentium dicta sequi quis hic eligendi illum in, officiis culpa quia fugit
+        molestias? Itaque doloribus praesentium fugiat!
+      </Paragraph>
+      <Button secondary>Remove</Button>
+    </InnerWrapper>
+  </StyledWrapper>
+);
+
+export default Card;
